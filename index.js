33f503aba756e19cf7697a8e18d2e673b343ae18
AnemayGi <giane.galhard@vtex.com>
2025-05-21 18:13:08 -0300

    refactor: index

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
 
 const usuariosRoutes = require('./src/usuarios/routes')
 
 const app = express();
-app.use(cors());
 
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Backend da Biblioteca do Cursinho. Feito por Ana Clara, Giane e Giovanna");
