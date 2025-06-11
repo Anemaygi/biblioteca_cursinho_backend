0fdbbce86eb32d39bdbc0b400d066bac88bdb06f
Ana Clara Segal <148593673+segalv@users.noreply.github.com>
2025-06-11 20:52:15 -0300

    Update utils.js

--- src/penalidades/utils.js
+++ src/penalidades/utils.js
@@ -1,8 +1,16 @@
-function formatarDataISO(date) {
-    return date.toISOString().slice(0, 10)
-  }
-  
-  module.exports = {
-    formatarDataISO
-  }
-  
+function formatarPenalidade(penalidade) {
+  return {
+    id: penalidade.id,
+    usuarioId: penalidade.usuario_id,
+    usuarioNome: penalidade.usuario_nome,
+    tipoId: penalidade.tipo_id,
+    tipo: penalidade.tipo,
+    causaId: penalidade.causa_id,
+    causa: penalidade.causa,
+    descricao: penalidade.descricao,
+    data: penalidade.data,
+    cumprida: penalidade.cumprida,
+  };
+}
+
+module.exports = { formatarPenalidade };
