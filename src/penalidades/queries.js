324df4b7d6076773edcc42e1fe9d5c36f332dedb
Ana Clara Segal <148593673+segalv@users.noreply.github.com>
2025-06-11 21:12:46 -0300

    Update queries.js

--- src/penalidades/queries.js
+++ src/penalidades/queries.js
@@ -1,41 +1,46 @@
 const getAll = `
-  SELECT p.*, u.nome AS usuario_nome, t.tipo, c.causa
+  SELECT p.*, u.nome AS usuario_nome, t.nome AS tipo, c.nome AS causa
   FROM penalidade p
   LEFT JOIN usuario u ON p.usuario_id = u.id
-  LEFT JOIN tipo_penalidade t ON p.tipo_id = t.id
-  LEFT JOIN causa_penalidade c ON p.causa_id = c.id
-  ORDER BY p.data DESC;
+  LEFT JOIN penalidade_tipo t ON p.tipo_id = t.id
+  LEFT JOIN penalidade_causa c ON p.causa_id = c.id
+  ORDER BY p.data_aplicacao DESC;
 `;
 
 const insert = `
-  INSERT INTO penalidade (usuario_id, tipo_id, causa_id, descricao, data, cumprida)
-  VALUES ($1, $2, $3, $4, $5, FALSE)
+  INSERT INTO penalidade (
+    usuario_id, exemplar_codigo, emprestimo_data_inicio, tipo_id, causa_id, data_aplicacao, data_suspensao, status_cumprida
+  ) VALUES ($1, $2, $3, $4, $5, $6, $7, FALSE)
   RETURNING *;
 `;
 
 const update = `
   UPDATE penalidade
-  SET usuario_id = $1, tipo_id = $2, causa_id = $3, descricao = $4, data = $5, cumprida = $6
-  WHERE id = $7
+  SET tipo_id = $1, causa_id = $2, data_suspensao = $3, status_cumprida = $4
+  WHERE usuario_id = $5 AND exemplar_codigo = $6 AND emprestimo_data_inicio = $7 AND data_aplicacao = $8
   RETURNING *;
 `;
 
 const remove = `
   DELETE FROM penalidade
-  WHERE id = $1
+  WHERE usuario_id = $1 AND exemplar_codigo = $2 AND emprestimo_data_inicio = $3 AND data_aplicacao = $4
   RETURNING *;
 `;
 
 const markCumprida = `
   UPDATE penalidade
-  SET cumprida = TRUE
-  WHERE id = $1
+  SET status_cumprida = TRUE
+  WHERE usuario_id = $1 AND exemplar_codigo = $2 AND emprestimo_data_inicio = $3 AND data_aplicacao = $4
   RETURNING *;
 `;
 
 const getByUsuario = `
-  SELECT * FROM penalidade
-  WHERE usuario_id = $1;
+  SELECT p.*, t.nome AS tipo, c.nome AS causa
+  FROM penalidade p
+  LEFT JOIN penalidade_tipo t ON p.tipo_id = t.id
+  LEFT JOIN penalidade_causa c ON p.causa_id = c.id
+  WHERE usuario_id = $1
+  ORDER BY p.data_aplicacao DESC;
 `;
 
 module.exports = {
