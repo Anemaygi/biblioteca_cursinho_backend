84e761e100fccd822d8b9b63cac1a245ff713b0f
Ana Clara Segal <148593673+segalv@users.noreply.github.com>
2025-06-19 19:48:05 -0300

    Update routes.js
    adiciona back para consultar penalidades_tipo e penalidades_causa

--- src/penalidades/routes.js
+++ src/penalidades/routes.js
@@ -9,5 +9,8 @@ router.get('/usuario/:usuario_id', controller.getByUsuario);
 router.put('/:usuario_id/:exemplar_codigo/:emprestimo_data_inicio/:data_aplicacao', controller.editPenalidade);
 router.delete('/:usuario_id/:exemplar_codigo/:emprestimo_data_inicio/:data_aplicacao', controller.removePenalidade);
 router.patch('/:usuario_id/:exemplar_codigo/:emprestimo_data_inicio/:data_aplicacao/cumprida', controller.marcarCumprida);
+router.get('/tipos', controller.getTipos)
+router.get('/causas', controller.getCausas)
+
 
 module.exports = router;
