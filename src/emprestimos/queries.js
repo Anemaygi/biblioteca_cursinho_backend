60f5fbfd94c17d571c4982fb61b72ce73ac84e29
segalv <anaclarasvp@usp.br>
2025-06-20 12:04:20 -0300

    atualiza o back end para as novas requisições feitas no front

--- src/emprestimos/queries.js
+++ src/emprestimos/queries.js
@@ -1,43 +1,19 @@
-const getAll = `
-  SELECT * FROM emprestimo ORDER BY data_inicio DESC;
-`;
-
-const getByUsuarioOuLivro = `
-  SELECT * FROM emprestimo
-  WHERE usuario_id::text ILIKE $1 OR exemplar_codigo ILIKE $1;
-`;
-
-const insert = `
-  INSERT INTO emprestimo (usuario_id, exemplar_codigo, data_inicio, data_fim_previsto)
-  VALUES ($1, $2, CURRENT_DATE, CURRENT_DATE + INTERVAL '10 days')
-  RETURNING *;
-`;
-
-const remove = `
-  DELETE FROM emprestimo
-  WHERE usuario_id = $1 AND exemplar_codigo = $2 AND data_inicio = $3;
-`;
-
-const renovar = `
-  UPDATE emprestimo
-  SET data_fim_previsto = data_fim_previsto + INTERVAL '10 days',
-      renovado = TRUE
-  WHERE usuario_id = $1 AND exemplar_codigo = $2 AND data_inicio = $3
-  RETURNING *;
-`;
-
-const update = `
-  UPDATE emprestimo
-  SET data_fim_previsto = $4, data_devolucao = $5
-  WHERE usuario_id = $1 AND exemplar_codigo = $2 AND data_inicio = $3
-  RETURNING *;
-`;
+const getAllEmprestimos = `
+  SELECT
+    e.usuario_id,
+    e.exemplar_codigo,
+    e.data_inicio,
+    e.data_fim_previsto,
+    e.data_devolucao,
+    e.renovado,
+    u.nome AS usuario_nome,
+    l.titulo AS livro_titulo
+  FROM emprestimo e
+  JOIN usuario u ON e.usuario_id = u.id
+  JOIN exemplar ex ON e.exemplar_codigo = ex.codigo
+  JOIN livro l ON ex.livro_id = l.id;
+`
 
 module.exports = {
-  getAll,
-  getByUsuarioOuLivro,
-  insert,
-  remove,
-  renovar,
-  update
+  getAllEmprestimos
 }
