6bbe625b9cf996bdbb8c5d46c94b638bb6dbc1d1
segalv <anaclarasvp@usp.br>
2025-06-20 18:13:34 -0300

    update backend reservas para a página de reservas

--- src/reservas/routes.js
+++ src/reservas/routes.js
@@ -5,5 +5,7 @@ const router = Router();
 
 router.post('/', controller.createReserva);
 router.get('/', controller.getAllReservas);
+router.get('/livros-disponiveis', controller.getLivrosComReservasEDisponiveis);
+router.get('/livro/:livroId', controller.getReservasPorLivroId);
 
 module.exports = router;
